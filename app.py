71c470ab012a4ca14eca6e4fe5b538e395242c65
Engelbert Juárez <engel24jb@yahoo.com>
2021-08-03 16:33:38 -0600

    created binance webhook

---
+++ app.py
@@ -0,0 +1,51 @@
+import json, config
+from flask import Flask, request
+from binance.client import Client
+from binance.enums import *
+
+app = Flask(__name__)
+
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
+
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+    try:
+        print(f'sending order {order_type} - {side} {quantity} {symbol}')
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+    except Exception as e:
+        print('an exception occured - {}'.format(e))
+        return False
+    return order
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # print(request.data)
+    data = json.loads(request.data)
+
+    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
+        return {
+            "code": "error",
+            "message": "Nice try, invalid passphrase"
+        }
+
+    print(data['ticker'])
+    print(data['bar'])
+    
+    side = data['strategy']['order_action'].upper()
+    quantity = data['strategy']['order_contracts']
+    order_response = order(side, quantity, 'DOGEUSD')
+
+    if order_response:
+        return {
+            'code': 'success',
+            'message': 'order executed'
+        }
+    else:
+        print('order failed')
+        return {
+            'code': 'error',
+            'message': 'order failed'
+        }
