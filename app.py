3f02005fda4292fa74d46ba0c52ab8bc038b83ca
Engelbert Juárez <engel24jb@yahoo.com>
2021-12-17 18:45:02 -0600

    arreglado ticker incorrecto y problema con las api keys

--- app.py
+++ app.py
@@ -5,47 +5,44 @@ from binance.enums import *
 
 app = Flask(__name__)
 
-client = Client(config.API_KEY, config.API_SECRET, tld='us')
+client = Client(config.API_KEY, config.API_SECRET)
+
 
 def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
-        print(f'sending order {order_type} - {side} {quantity} {symbol}')
-        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.create_order(
+            symbol=symbol, side=side, type=order_type, quantity=quantity
+        )
     except Exception as e:
-        print('an exception occured - {}'.format(e))
+        print("an exception occured - {}".format(e))
         return False
     return order
 
-@app.route('/')
-def welcome():
-    return render_template('index.html')
 
-@app.route('/webhook', methods=['POST'])
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+
+@app.route("/webhook", methods=["POST"])
 def webhook():
     # print(request.data)
     data = json.loads(request.data)
 
-    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
-        return {
-            "code": "error",
-            "message": "Nice try, invalid passphrase"
-        }
+    if data["passphrase"] != config.WEBHOOK_PASSPHRASE:
+        return {"code": "error", "message": "Nice try, invalid passphrase"}
 
-    print(data['ticker'])
-    print(data['bar'])
-    
-    side = data['strategy']['order_action'].upper()
-    quantity = data['strategy']['order_contracts']
-    order_response = order(side, quantity, 'DOGEUSD')
+    print(data["ticker"])
+    print(data["bar"])
+
+    side = data["strategy"]["order_action"].upper()
+    quantity = data["strategy"]["order_contracts"]
+    ticker = data["ticker"].upper()
+    order_response = order(side, quantity, ticker)
 
     if order_response:
-        return {
-            'code': 'success',
-            'message': 'order executed'
-        }
+        return {"code": "success", "message": "order executed"}
     else:
-        print('order failed')
-        return {
-            'code': 'error',
-            'message': 'order failed'
-        }
+        print("order failed")
+        return {"code": "error", "message": "order failed"}
